3dd6a5b93afba26273be29cd4717f75ed68d0c83
tduncan14 <nbatre1@yahoo.com>
2023-05-13 02:13:08 -0700

    keep moving forward

---
+++ commercial/backend/seeder.js
@@ -0,0 +1,100 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import products from './data/products';
+import users  from './data/users'
+import User from './models/userModel';
+import Order from './models/orderModel';
+import Product from './models/productModel';
+import connectDB from './config/db.js';
+
+
+dotenv.config()
+
+
+connectDB()
+
+
+const importData = async () => {
+
+
+    try{
+        await Order.deleteMany();
+        await Product.deleteMany ();
+        await User.deleteMany();
+
+
+        const createdUsers = await User.insertMany(users);
+
+        const adminUser  = createdUsers[0]._id
+
+
+        const sampleProducts = product.map((product) =>{
+
+            return {...product, user:adminUser}
+        })
+
+
+        await Product.insertMany(sampleProducts);
+
+
+        console.log('data Imported'.green.inverse)
+        process.exit()
+
+    }
+
+    catch(error){
+
+        console.error(`${err}`.red.inverse)
+        process.exit(1)
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+// destroy the data
+
+
+const destroyData = async () => {
+
+
+    try{
+
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+
+        console.log(`Data Destroyed`.red.inverse)
+        process.exit()
+
+    }
+
+    catch(error){
+
+
+        console.error(`${error}`.red.inverse);
+        process.exit(1)
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
