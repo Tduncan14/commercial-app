f6d41036411b72cc9689a9e1b5d6e512f4249113
tduncan14 <nbatre1@yahoo.com>
2023-06-02 18:38:08 -0700

    keep moving forward

--- commercial/backend/routes/userRoutes.js
+++ commercial/backend/routes/userRoutes.js
@@ -12,15 +12,15 @@ import express from 'express';
     getUsers
 
   } from '../controllers/userController.js';
+import { admin,protect } from '../middleware/authMiddleware.js';
 
 
 
-
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect,admin,getUsers);
 router.post('/logout',logoutUser);
 router.post('/login',authUser)
-router.route('/profile').get(getUserProfile).put(updateUser)
-router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser)
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUser)
+router.route('/:id').delete(deleteUser).get(getUserById).put(protect,updateUser)
 
 
 
